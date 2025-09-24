5616782784bc429ef0442018bd12f1cc43621839
malikfaisal11 <malikfaisal11786@gmail.com>
2025-09-24 06:07:08 +0500

    chanded the code

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from GitHub Actions!"
+    return "Hello from Flask on EC2 with Nginx + GitHub Actions!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
